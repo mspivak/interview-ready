977bfd97c230332d0ad448a8e82150edcfff633f
Mauro Spivak <mauros@gmail.com>
2025-02-05 15:49:40 -0300

    08. zeroMatrix

--- technical-fundamentals/coding/problems/08_zeroMatrix.ts
+++ technical-fundamentals/coding/problems/08_zeroMatrix.ts
@@ -2,8 +2,27 @@
 
 // Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
 
-type Matrix = number[][]
+type Matrix = number[][];
 
-export default function zeroMatrix (matrix: Matrix) {
+export default function zeroMatrix(matrix: Matrix) {
+	const zeroesCols: number[] = [];
+	const zeroesRows: number[] = [];
 
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (matrix[i][j] == 0) {
+				zeroesRows.push(i);
+				zeroesCols.push(j);
+			}
+		}
+	}
+
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			matrix[i][j] =
+				zeroesRows.includes(i) || zeroesCols.includes(j) ? 0 : matrix[i][j];
+		}
+	}
+
+	return matrix;
 }
