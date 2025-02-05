50b84af71255d302f3cbcc29e7308a20931a80f3
Mauro Spivak <mauros@gmail.com>
2025-02-05 12:10:36 -0300

    04. palindromePermutations

--- technical-fundamentals/coding/problems/04_palindromePermutation.ts
+++ technical-fundamentals/coding/problems/04_palindromePermutation.ts
@@ -1,4 +1,4 @@
-// 4. *Palindrome Permutation*: 
+// 4. *Palindrome Permutation*:
 
 // Given a string, write a function to check if it is a permutation of a palindrome.
 // A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
@@ -9,6 +9,28 @@
 // Output True (permutations: "taco cat", "atco cta", etc.)
 // ```
 
-export default function palindromePermutation (str: string): boolean {
+export default function palindromePermutation(str: string): boolean {
+	const seen = new Map<string, number>();
 
+	for (let i = 0; i < str.length; i++) {
+		const c = str[i].toLowerCase();
+		if (c == " ") {
+			continue;
+		}
+		const count = seen.get(c);
+		seen.set(c, count ? count + 1 : 1);
+	}
+
+	let middleFound = false;
+	for (const [key, value] of seen.entries()) {
+		if (value % 2 == 0) {
+			continue;
+		}
+		if (middleFound) {
+			return false;
+		}
+		middleFound = true;
+	}
+
+	return true;
 }
