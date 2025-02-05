7df6b4915d6a3f3b1828e4bd41ee667cf9a3e76d
Mauro Spivak <mauros@gmail.com>
2025-02-05 15:06:40 -0300

    07. rotateMatrix

--- technical-fundamentals/coding/problems/07_rotateMatrix.ts
+++ technical-fundamentals/coding/problems/07_rotateMatrix.ts
@@ -3,8 +3,30 @@
 // Given an image represented by an NxN matrix, where each pixel in the image is 4
 // bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
-type Matrix = number[][]
+type Matrix = number[][];
 
-export default function rotateMatrix (matrix: Matrix) {
-
+export default function rotateMatrix(matrix: Matrix) {
+	console.log(matrix);
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = i + 1; j < matrix.length; j++) {
+			const v = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = v;
+		}
+	}
+	matrix.forEach((row) => row.reverse());
+	return matrix;
 }
+
+// export default function rotateMatrix(matrix: Matrix) {
+// 	const newMatrix: Matrix = [];
+// 	for (let i = 0; i < matrix.length; i++) {
+// 		for (let j = 0; j < matrix.length; j++) {
+// 			if (newMatrix[i] === undefined) {
+// 				newMatrix[i] = [];
+// 			}
+// 			newMatrix[i][matrix.length - 1 - j] = matrix[j][i];
+// 		}
+// 	}
+// 	matrix = newMatrix;
+// }
