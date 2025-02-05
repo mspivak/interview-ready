909c3f35b5b98949cc3b4918cf4de20f20bc0849
Mauro Spivak <mauros@gmail.com>
2025-02-05 12:07:18 -0300

    03. URLify

--- technical-fundamentals/coding/problems/03_urlify.ts
+++ technical-fundamentals/coding/problems/03_urlify.ts
@@ -4,6 +4,11 @@
 // You may assume that the string has sufficient space at the end to hold the additional characters,
 // and that you are given the "true" length of the string.
 
-export default function URLify (s1 : string): string {
-
+export default function URLify(s1: string): string {
+	let result = "";
+	for (let i = 0; i < s1.length; i++) {
+		const c = s1[i];
+		result += c == " " ? "%20" : c;
+	}
+	return result;
 }
