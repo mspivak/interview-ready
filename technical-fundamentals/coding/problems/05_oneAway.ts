cdcca4e3639b90e9606c940b40b289e2f980994d
Mauro Spivak <mauros@gmail.com>
2025-02-05 13:38:34 -0300

    05. oneAway

--- technical-fundamentals/coding/problems/05_oneAway.ts
+++ technical-fundamentals/coding/problems/05_oneAway.ts
@@ -5,5 +5,36 @@
 // Given two strings, write a function to check if they are one edit (or zero edits) away.
 
 export default function isOneAway(str1: string, str2: string): boolean {
-  
+	if (Math.abs(str1.length - str2.length) > 1) return false;
+
+	let i1 = 0;
+	let i2 = 0;
+	let editCount = 0;
+
+	while (str1[i1] && str2[i2]) {
+		if (str1[i1] !== str2[i2]) {
+			editCount++;
+			if (str1[i1] == str2[i1 + 1]) {
+				i2++;
+				continue;
+			}
+			if (str1[i1 + 1] == str2[i2]) {
+				i1++;
+				continue;
+			}
+			if (str1[i1 + 1] == str2[i1 + 1]) {
+				i1++;
+				i2++;
+				continue;
+			}
+		}
+
+		i1++;
+		i2++;
+
+		if (editCount > 1) {
+			return false;
+		}
+	}
+	return true;
 }
