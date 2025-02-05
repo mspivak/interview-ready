c01585431f5281dec3756d19cf154c7fc8dfae0c
Mauro Spivak <mauros@gmail.com>
2025-02-05 12:04:28 -0300

    02. checkPermutations

--- technical-fundamentals/coding/problems/02_checkPermutations.ts
+++ technical-fundamentals/coding/problems/02_checkPermutations.ts
@@ -2,4 +2,15 @@
 
 // Given two strings, write a method to decide if one is a permutation of the other.
 
-export default function checkPermutations(s1: string, s2: string): boolean {}
+export default function checkPermutations(s1: string, s2: string): boolean {
+	if (s1.length !== s2.length) {
+		return false;
+	}
+
+	for (let i = 0; i < s1.length; i++) {
+		if (s2.indexOf(s1[i]) === -1) {
+			return false;
+		}
+	}
+	return true;
+}
