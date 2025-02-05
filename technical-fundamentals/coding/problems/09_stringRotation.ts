aff972b82c892d2e18c99b77fb75eff9e9b0ea17
Mauro Spivak <mauros@gmail.com>
2025-02-05 17:25:42 -0300

    fix test and 09. stringRotation

--- technical-fundamentals/coding/problems/09_stringRotation.ts
+++ technical-fundamentals/coding/problems/09_stringRotation.ts
@@ -1,13 +1,25 @@
 // 9. *String Rotation*;
 
 // Assume you have a method isSubstring which checks if one word is a substring of another.
-// Given two strings, si and s2, write code to check if s2 is a rotation of 1i using only one call to isSubstring
+// Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
 // [e.g., "waterbottle" is a rotation oP'erbottlewat")
 
-function isSubstring(s1: string, s2: string): boolean {
-    return s1.includes(s2);
-}
+export default function stringRotation(
+	s1: string,
+	s2: string,
+	isSubstring: Function
+): boolean {
+	if (s1.length !== s2.length) {
+		return false;
+	}
 
-export default function stringRotation(s1: string, s2: string): boolean {
+	let r = s2;
 
+	for (let i = 0; i < s1.length; i++) {
+		r = r.slice(1, s1.length) + r[0];
+		if (r == s2) {
+			return isSubstring(r, s2);
+		}
+	}
+	return false;
 }
--- technical-fundamentals/coding/problems/__tests__/strings/09_stringRotation.test.ts
+++ technical-fundamentals/coding/problems/__tests__/strings/09_stringRotation.test.ts
@@ -1,17 +1,26 @@
 import stringRotation from "../../09_stringRotation";
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+function isSubstring(s1: string, s2: string): boolean {
+	return s1.includes(s2);
+}
 
 describe("09 - stringRotation", () => {
-  test("rotates a string", () => {
-    const str1 = "Hello";
-    const str2 = "oHell";
-    const result = stringRotation(str1, str2);
-    expect(result).toEqual(true);
-  });
+	test("rotates a string", () => {
+		const spy = vi.fn(isSubstring);
+		const str1 = "Hello";
+		const str2 = "oHell";
+		const result = stringRotation(str1, str2, spy);
+		expect(spy).toHaveBeenCalledOnce();
+		expect(result).toEqual(true);
+	});
 
-  test("rotates another string", () => {
-    const str1 = "waterbottle";
-    const str2 = "erbottlewat";
-    const result = stringRotation(str1, str2);
-    expect(result).toEqual(true);
-  });
+	test("rotates another string", () => {
+		const spy = vi.fn(isSubstring);
+		const str1 = "waterbottle";
+		const str2 = "erbottlewat";
+		const result = stringRotation(str1, str2, spy);
+		expect(spy).toHaveBeenCalledOnce();
+		expect(result).toEqual(true);
+	});
 });
