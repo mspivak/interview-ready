cc8a4962f1c4720ba53341e3eba9fe5471dbed8d
Mauro Spivak <mauros@gmail.com>
2025-02-05 11:54:03 -0300

    01. isUnique

--- technical-fundamentals/coding/problems/01_isUnique.ts
+++ technical-fundamentals/coding/problems/01_isUnique.ts
@@ -3,4 +3,14 @@
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
 
-export default function isUnique(str: string): boolean {}
+export default function isUnique(str: string): boolean {
+	const seen: Set<string> = new Set();
+	for (let i = 0; i < str.length; i++) {
+		const c = str[i];
+		if (seen.has(c)) {
+			return false;
+		}
+		seen.add(c);
+	}
+	return true;
+}
