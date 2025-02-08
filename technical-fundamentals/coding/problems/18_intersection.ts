6dcac466f9f185cb1244b1d9eada2f0e013a22ca
Mauro Spivak <mauros@gmail.com>
2025-02-08 14:22:30 -0300

    18. intersection

--- technical-fundamentals/coding/problems/18_intersection.ts
+++ technical-fundamentals/coding/problems/18_intersection.ts
@@ -7,11 +7,24 @@
 import { LinkedList } from "./10_LinkedList";
 
 export type Node<T> = {
-  value: T;
-  next?: Node<T>;
+	value: T;
+	next?: Node<T>;
 };
 
 export default function intersection<T>(
-  list1: Node<T> | undefined,
-  list2: Node<T> | undefined,
-): Node<T> | undefined {}
+	list1: Node<T> | undefined,
+	list2: Node<T> | undefined
+): Node<T> | undefined {
+	let p1: Node<T> | undefined = list1;
+	while (p1) {
+		let p2: Node<T> | undefined = list2;
+		while (p2) {
+			if (p1 == p2) {
+				return p1;
+			}
+			p2 = p2.next;
+		}
+		p1 = p1.next;
+	}
+	return undefined;
+}
