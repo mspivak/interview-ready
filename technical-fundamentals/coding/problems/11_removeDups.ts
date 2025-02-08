4f02ddb831fc7b01d8a19a82d04ae4a60a8bed3f
Mauro Spivak <mauros@gmail.com>
2025-02-08 01:00:27 -0300

    11. removeDups

--- technical-fundamentals/coding/problems/11_removeDups.ts
+++ technical-fundamentals/coding/problems/11_removeDups.ts
@@ -8,8 +8,22 @@
 import { LinkedList } from "./10_LinkedList";
 
 export type Node<T> = {
-  value: T;
-  next?: Node<T>;
+	value: T;
+	next?: Node<T>;
 };
 
-export default function removeDups<T>(head?: Node<T>): Node<T> | undefined {}
+export default function removeDups<T>(head?: Node<T>): Node<T> | undefined {
+	let p = head;
+	let lastDifferent = p;
+	let seen = new Set();
+	while (p) {
+		if (seen.has(p.value)) {
+			if (lastDifferent) lastDifferent.next = p.next;
+		} else {
+			seen.add(p.value);
+			lastDifferent = p;
+		}
+		p = p.next;
+	}
+	return head;
+}
