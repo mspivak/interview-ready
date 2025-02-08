74123249782d7b73915d0d7adbaef4cc15464f6f
Mauro Spivak <mauros@gmail.com>
2025-02-08 01:00:58 -0300

    12. kthToLast

--- technical-fundamentals/coding/problems/12_kthToLast.ts
+++ technical-fundamentals/coding/problems/12_kthToLast.ts
@@ -5,11 +5,31 @@
 import { LinkedList } from "./10_LinkedList";
 
 export type Node<T> = {
-  value: T;
-  next?: Node<T>;
+	value: T;
+	next?: Node<T>;
 };
 
 export default function kthToLast<T>(
-  head: Node<T>,
-  k: number,
-): Node<T> | undefined {}
+	head: Node<T>,
+	k: number
+): Node<T> | undefined {
+	let p: Node<T> | undefined = head;
+	let count = 0;
+	let i = 0;
+	while (p) {
+		count++;
+		p = p.next;
+	}
+
+	if (k > count) {
+		return undefined;
+	}
+
+	p = head;
+	while (i < count - k) {
+		i++;
+		p = p?.next;
+	}
+
+	return p;
+}
