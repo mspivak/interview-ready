e2ccd3c6cdb390311cbe950dc6dbadadb8bf2dec
Mauro Spivak <mauros@gmail.com>
2025-02-08 13:51:37 -0300

    16. sumListsForwardOrder

--- technical-fundamentals/coding/problems/16_sumListsForwardOrder.ts
+++ technical-fundamentals/coding/problems/16_sumListsForwardOrder.ts
@@ -9,18 +9,25 @@
 
 // ```
 // EXAMPLE
-// Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).Thatis,617 + 295
-// Output:9 -> 1 -> 2,Thatis,912.
+// Input: (6 -> 1 -> 7) + (2 -> 9 -> 5). That is, 617 + 295
+// Output:9 -> 1 -> 2. That is, 912.
 // ```
 
 import { LinkedList } from "./10_LinkedList";
+import sumLists from "./15_sumLists";
 
 export type Node<T> = {
-  value: T;
-  next?: Node<T>;
+	value: T;
+	next?: Node<T>;
 };
 
 export default function sumListsForwardOrder(
-  list1: Node<number> | undefined,
-  list2: Node<number> | undefined,
-): Node<number> | undefined {}
+	list1: Node<number> | undefined,
+	list2: Node<number> | undefined
+): Node<number> | undefined {
+	const reversed1 = new LinkedList(list1).reverse();
+	const reversed2 = new LinkedList(list2).reverse();
+	const sum = sumLists(reversed1.head, reversed2.head);
+	const reversedSum = new LinkedList(sum).reverse();
+	return reversedSum.head;
+}
