01e85c5970022c140f5b54d68d5d339b0ab23621
Mauro Spivak <mauros@gmail.com>
2025-02-09 15:27:40 -0300

    30. trees

--- technical-fundamentals/coding/problems/30_trees.ts
+++ technical-fundamentals/coding/problems/30_trees.ts
@@ -1,13 +1,39 @@
 // Write the basic tree algorithms of Depth-first-search and Breadth-first search.
 
+import { LinkedList } from "./10_LinkedList";
+
 export type TreeNode<T> = {
-  value: T;
-  left?: TreeNode<T>;
-  right?: TreeNode<T>;
+	value: T;
+	left?: TreeNode<T>;
+	right?: TreeNode<T>;
 };
 
 export class Tree<T> {
-  bfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {}
+	queue: LinkedList<TreeNode<T>>;
 
-  dfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {}
+	constructor() {
+		this.queue = new LinkedList<TreeNode<T>>();
+	}
+
+	bfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {
+		if (!node) {
+			return;
+		}
+		this.queue.push(node);
+
+		this.queue.forEach((node: TreeNode<T>) => {
+			visit(node);
+			if (node.left) this.queue.push(node.left);
+			if (node.right) this.queue.push(node.right);
+		});
+	}
+
+	dfs(node: TreeNode<T> | undefined, visit: (node: TreeNode<T>) => void) {
+		if (!node) {
+			return;
+		}
+		visit(node);
+		this.dfs(node.left, visit);
+		this.dfs(node.right, visit);
+	}
 }
