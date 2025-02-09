d1f7cb6dcf8d1b2cd9955533ab1dbb5781658e58
Mauro Spivak <mauros@gmail.com>
2025-02-09 11:37:58 -0300

    21. threeStacks

--- technical-fundamentals/coding/problems/21_threeStacks.ts
+++ technical-fundamentals/coding/problems/21_threeStacks.ts
@@ -1,17 +1,54 @@
 // 1. *Three in One*: Describe how you could use a single array to implement three stacks.
 
 export default class ThreeStacks<T> {
-    private array: T[];
+	private array: (T | undefined)[];
+	private stackSize: number;
 
-    constructor(arrayLength: number) {
-    }
+	constructor(arrayLength: number) {
+		this.array = Array(arrayLength).fill(undefined);
+		this.stackSize = Math.floor(arrayLength / 3);
+	}
 
-    push(stackNum: number, value: T): void {
-    }
+	stackBounds(stackNum: number) {
+		const from = Math.floor(this.stackSize * stackNum);
+		const to = Math.floor(this.stackSize * (stackNum + 1));
+		return [from, to];
+	}
 
-    pop(stackNum: number): T | undefined {
-    }
+	push(stackNum: number, value: T): void {
+		const [from, to] = this.stackBounds(stackNum);
 
-    peek(stackNum: number): T | undefined {
-    }
+		let i = from;
+		while (this.array[i] && i !== to) {
+			i++;
+		}
+
+		if (this.array[i] === undefined) {
+			this.array[i] = value;
+		} else {
+			throw new Error(`Stack size of ${this.stackSize} exeeded.`);
+		}
+	}
+
+	pop(stackNum: number): T | undefined {
+		const [from, to] = this.stackBounds(stackNum);
+		let result: T | undefined;
+		let i = from;
+		while (this.array[i] && i !== to) {
+			i++;
+		}
+		result = this.array[i - 1];
+		this.array[i - 1] = undefined;
+		return result;
+	}
+
+	peek(stackNum: number): T | undefined {
+		const [from, to] = this.stackBounds(stackNum);
+		let result: T | undefined;
+		let i = from;
+		while (this.array[i] && i !== to) {
+			i++;
+		}
+		return this.array[i - 1];
+	}
 }
