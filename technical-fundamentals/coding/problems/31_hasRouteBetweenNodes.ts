d553b6d45c5b11a03e116f97a60572801aaf0077
Mauro Spivak <mauros@gmail.com>
2025-02-09 16:02:05 -0300

    31. hasRouteBetweenNodes

--- technical-fundamentals/coding/problems/31_hasRouteBetweenNodes.ts
+++ technical-fundamentals/coding/problems/31_hasRouteBetweenNodes.ts
@@ -4,11 +4,26 @@
 // between two nodes.
 
 export type GraphNode = {
-  value: number;
-  neighbors: GraphNode[];
+	value: number;
+	neighbors: GraphNode[];
 };
 
 export default function hasRouteBetweenNodes(
-  start: GraphNode,
-  end: GraphNode,
-): boolean {}
+	start: GraphNode,
+	end: GraphNode,
+	visited: GraphNode[] = []
+): boolean {
+	if (start.value == end.value) {
+		return true;
+	}
+	visited.push(start);
+	for (const neighbor of start.neighbors) {
+		if (
+			!visited.includes(neighbor) &&
+			hasRouteBetweenNodes(neighbor, end, visited)
+		) {
+			return true;
+		}
+	}
+	return false;
+}
