c5765d87f7d80003f6b2819f3aecb43e22f62106
Mauro Spivak <mauros@gmail.com>
2025-02-10 10:10:29 -0300

    34. checkBalanced

--- technical-fundamentals/coding/problems/34_checkBalanced.ts
+++ technical-fundamentals/coding/problems/34_checkBalanced.ts
@@ -5,11 +5,31 @@
 // such that the heights of the two subtrees of any node never differ by more than one.
 
 export type TreeNode<T> = {
-    value: T;
-    left?: TreeNode<T>;
-    right?: TreeNode<T>;
+	value: T;
+	left?: TreeNode<T>;
+	right?: TreeNode<T>;
 };
 
 export default function checkBalanced<T>(tree?: TreeNode<T> | null): boolean {
+	const depths: number[] = [];
+	let depth = 0;
+	if (!tree) return true;
+	dfs(tree, depth);
 
+	function dfs(tree: TreeNode<T> | undefined, depth: number) {
+		if (!tree) return;
+		if (!depths[depth]) depths[depth] = 0;
+		depths[depth]++;
+		depth++;
+		dfs(tree.left, depth);
+		dfs(tree.right, depth);
+	}
+
+	for (let i = 0; i < depths.length; i++) {
+		if (depths[i] == 2 ** i) {
+			continue;
+		}
+		return i === depths.length - 1;
+	}
+	return true;
 }
