cdf15394a17aa0147914d839ac761475e33202ec
Mauro Spivak <mauros@gmail.com>
2025-02-10 11:32:46 -0300

    35. validateBST

--- technical-fundamentals/coding/problems/35_validateBST.ts
+++ technical-fundamentals/coding/problems/35_validateBST.ts
@@ -1,13 +1,26 @@
 // 5. *Validate BST*:
 
+import { nodeModuleNameResolver } from "typescript";
+
 // Implement a function to check if a binary tree is a binary search tree.
 
 export type TreeNode<T> = {
-  value: T;
-  left?: TreeNode<T>;
-  right?: TreeNode<T>;
+	value: T;
+	left?: TreeNode<T>;
+	right?: TreeNode<T>;
 };
 
 export default function validateBST<T>(
-  node: TreeNode<T> | undefined,
-): boolean {}
+	node: TreeNode<T> | undefined,
+	min: T | null = null,
+	max: T | null = null
+): boolean {
+	if (!node) return true;
+	if ((min && node.value < min) || (max && node.value > max)) {
+		return false;
+	}
+	return (
+		validateBST(node.left, min, node.value) &&
+		validateBST(node.right, node.value, max)
+	);
+}
