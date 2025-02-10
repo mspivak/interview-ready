0a11bd45f108d41418af705903b8c073fcf9ff8d
Mauro Spivak <mauros@gmail.com>
2025-02-10 15:34:44 -0300

    38. firstCommonAncestor

--- technical-fundamentals/coding/problems/38_firstCommonAncestor.ts
+++ technical-fundamentals/coding/problems/38_firstCommonAncestor.ts
@@ -5,15 +5,36 @@
 // NOTE: This is not necessarily a binary search tree.
 
 export type TreeNode<T> = {
-  value: T;
-  left?: TreeNode<T>;
-  right?: TreeNode<T>;
+	value: T;
+	left?: TreeNode<T>;
+	right?: TreeNode<T>;
 };
 
 export default function firstCommonAncestor<T>(
-  root: TreeNode<T> | undefined,
-  p: TreeNode<T>,
-  q: TreeNode<T>,
+	root: TreeNode<T> | undefined,
+	p: TreeNode<T>,
+	q: TreeNode<T>
 ): TreeNode<T> | undefined {
+	if (!root) {
+		return undefined;
+	}
 
+	if (p.value == root.value || q.value == root.value) {
+		return root;
+	}
+
+	const foundLeft = firstCommonAncestor(root.left, p, q);
+	const foundRight = firstCommonAncestor(root.right, p, q);
+
+	if (foundLeft && foundRight) {
+		return root;
+	}
+	if (foundLeft) {
+		return foundLeft;
+	}
+	if (foundRight) {
+		return foundRight;
+	}
+
+	return undefined;
 }
