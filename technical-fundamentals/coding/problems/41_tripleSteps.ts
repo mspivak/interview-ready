3ed66a3b902b1d728df04c28d30cb53ed80a012c
Mauro Spivak <mauros@gmail.com>
2025-02-11 19:12:14 -0300

    41. tripleSteps as top-down

--- technical-fundamentals/coding/problems/41_tripleSteps.ts
+++ technical-fundamentals/coding/problems/41_tripleSteps.ts
@@ -4,5 +4,25 @@
 // 1 step, 2 steps, or 3 steps at a time. Implement a method to count
 // how many possible ways the child can run up the stairs.
 //
-
-export default function tripleStep(n: number): number {}
+let tried = new Map();
+export default function tripleStep(n: number): number {
+	if (n <= 0) {
+		return 0;
+	}
+	if (tried.has(n)) {
+		return tried.get(n);
+	}
+	let possibleWays = 0;
+	for (const step of [1, 2, 3]) {
+		const newPosition = n - step;
+		if (newPosition == 0) {
+			possibleWays++;
+			continue;
+		} else if (newPosition < 0) {
+			continue;
+		}
+		possibleWays += tripleStep(newPosition);
+	}
+	tried.set(n, possibleWays);
+	return possibleWays;
+}
