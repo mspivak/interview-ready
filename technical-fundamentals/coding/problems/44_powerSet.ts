2ac450220006577e26f99397abc56a83b3e32aed
Mauro Spivak <mauros@gmail.com>
2025-02-24 16:59:34 -0300

    44. powerSet

--- technical-fundamentals/coding/problems/44_powerSet.ts
+++ technical-fundamentals/coding/problems/44_powerSet.ts
@@ -1,12 +1,22 @@
-// 4. *Power Set*: 
+// 4. *Power Set*:
 
 // Write a method to return all subsets of a set.
 
-// Example 
+// Example
 // Input: [1, 2, 3]
 // Output: [ [], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3] ];
 
-
 export function powerSet(set: number[]): number[][] {
-
+	if (set.length == 0) {
+		return [[]];
+	}
+	if (set.length == 1) {
+		return [[], set];
+	}
+	const lastSubsets = powerSet(set.slice(1, set.length));
+	const subsets = [...lastSubsets];
+	for (let i = 0; i < lastSubsets.length; i++) {
+		subsets.push([set[0], ...lastSubsets[i]]);
+	}
+	return subsets;
 }
